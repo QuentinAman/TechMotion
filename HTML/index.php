ace9bbb060fe8ecbe35e38ff435cad3dc7aa8561
QuentinAman <46739695+QuentinAman@users.noreply.github.com>
2019-01-26 08:16:08 +0100

    Update index.php

--- HTML/index.php
+++ HTML/index.php
@@ -12,7 +12,7 @@
     <?php
 	include("statut.php");
 	include("header.php");
-    $bdd = new PDO('mysql:host=localhost;dbname=id8487019_techmotion;charset=utf8', 'id8487019_quentinaman', 'chouchou');
+    $bdd = new PDO('mysql:host=localhost;dbname=article;charset=utf8', 'root', '');
 	$reponse = $bdd->query('SELECT * FROM articles ORDER BY id DESC LIMIT 9');
 
 	$donnees = $reponse->fetch()
